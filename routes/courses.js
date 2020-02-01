19370a31474ae76b21eeeea2e4d78ebef4620483
RaviC Rachamalla <rach0022@algonquinlive.com>
2020-02-01 14:11:16 -0500

    set base folder stucture, created 6 test students and courses

---
+++ routes/courses.js
@@ -0,0 +1,44 @@
+module.exports = [
+    {
+        id : 1345, 
+        code : "WZD", 
+        title : "Wizardry 101", 
+        description: "Learn various skills for all kinds of adminsitrative wizardry positions",
+        url: "http://school.wizard.com/wzd1345"
+    },
+    {
+        id : 1242, 
+        code : "BSN", 
+        title : "Financial Accounting 101", 
+        description: "Learn the basics of budgeting and more.",
+        url: "http://school.buisness.com/bsn1242"
+    },
+    {
+        id : 2376, 
+        code : "BSN", 
+        title : "Fundamentals of Buisness Law", 
+        description: "Introduction to the basics of buisness law",
+        url: "http://school.buisness.com/bsn2376"
+    },
+    {
+        id : 2402, 
+        code : "CMP", 
+        title : "Abstract Data Types and Algorithms", 
+        description: "Learn the basics of Big O thinking",
+        url: "http://school.technology.com/cmp2402"
+    },
+    {
+        id : 4562, 
+        code : "CKG", 
+        title : "Advanced Molecular Gastronomy", 
+        description: "Learn trade secrets from chefs all around about the new and exciting developments in the world of Molecular Gastronomy. Maltodextrin powder will not be supplied.",
+        url: "http://school.cooking.com/ckg4562"
+    },
+    {
+        id : 3429, 
+        code : "CAR", 
+        title : "Climate Control Systems", 
+        description: "Learn how to repair the various onboard climate control systems featured in all the newest cars.",
+        url: "http://school.repair.com/car3429"
+    }
+]
---
+++ routes/students.js
@@ -0,0 +1,44 @@
+module.exports = [
+    {
+        id: 884308,
+        firstName: "Jacob",
+        lastName: "Jones",
+        nickName: "JJ",
+        email: "jajo884@school.ca"
+    },
+    {
+        id: 563309,
+        firstName: "Rick",
+        lastName: "Deckard",
+        nickName: "Hunter",
+        email: "ride563@school.ca"
+    },
+    {
+        id: 587933,
+        firstName: "Barry",
+        lastName: "Allen",
+        nickName: "Flash",
+        email: "baal587@school.ca"
+    },
+    {
+        id: 765345,
+        firstName: "Melanie",
+        lastName: "Ting",
+        nickName: "Mel",
+        email: "meti765@school.ca"
+    },
+    {
+        id: 176487,
+        firstName: "Stan",
+        lastName: "Marsh",
+        nickName: "The Boy in the Red Poofball Hat",
+        email: "stma176@school.ca"
+    },
+    {
+        id: 465739,
+        firstName: "Jennifer",
+        lastName: "Gildenjhorn",
+        nickName: "Jenny",
+        email: "jegi465@school.ca"
+    },
+]
